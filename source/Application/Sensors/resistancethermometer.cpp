b2987e166b53bcbe9cdd55be0fa78991c21de7da
Art23urDav <ar4irrr23@gmail.com>
2024-05-04 17:11:43 -0800

    Добавил функцию Calculate

--- source/Application/Sensors/resistancethermometer.cpp
+++ source/Application/Sensors/resistancethermometer.cpp
@@ -9,7 +9,11 @@ void ResistanceThermometer::NotifyDataArrived()
 
 float ResistanceThermometer::Calculate(std::uint32_t adcCode)
 {
-  return adcCode;
+  voltage = (maxVoltageAdc * adcCode) / maxAdcCode;
+  koefK = (maxTempResistor - minTempResistor) / (maxVoltageResistor - minVoltageResistor);
+  koefB = maxVoltageResistor - (koefK - maxTempResistor);
+  mValue = koefK * voltage + koefB; 
+  return mValue;
 }
 
 float ResistanceThermometer::GetData() const
--- source/Application/Sensors/resistancethermometer.h
+++ source/Application/Sensors/resistancethermometer.h
@@ -16,8 +16,18 @@ class ResistanceThermometer : public IInjectedChannelNotifier, public IFloatData
     float GetData() const override;
   private:
     IDataProvider& mDataProvider;
+    std::uint32_t mAdcCode = 0U;
+    float maxAdcCode = 4096.0f;
+    float maxVoltageAdc = 3.0f;
     float mValue = 0.0f;
     float Calculate(std::uint32_t adcCode);
+    float voltage = 0.0f;
+    float koefK = 0.0f;
+    float koefB = 0.0f;
+    float maxTempResistor = 150.0f; 
+    float minTempResistor = 0.0f; 
+    float maxVoltageResistor = 2.0f; // TODO measure experimentally
+    float minVoltageResistor = 1.0f; // TODO measure experimentally
     
 };
 
